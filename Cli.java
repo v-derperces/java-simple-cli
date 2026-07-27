3e1e486ee8c914916de342bfd59af8a997896a80
Viviane-D <dervi.crazy@gmail.com>
2026-07-27 20:34:05 +0200

    add os command

--- Cli.java
+++ Cli.java
@@ -29,6 +29,9 @@ public class Cli {
 				System.out.println(System.getProperties().getProperty("user.name"));
 			}else if (command.equals("userhome")){
 				System.out.println(System.getProperties().getProperty("user.home"));
+			}else if(command.equals("os")){
+				System.out.println(System.getProperties().getProperty("os.name") + " (" + System.getProperties().getProperty("os.version") + ")");
+
 			}else {
 				// String concatenation
 				output = "Command '" + command + "' not found.";
