6b09c4e5ce3a1ea2ee6f2ac0c90309dc7d38b1ba
Viviane Derperces <vivianed167@outlook.com>
2026-07-27 15:49:15 +0200

    Initial commit

---
+++ Cli.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Cli {
+
+    // The main method is the entry point of the program. Rules regarding the main method:
+    //     - public: so the JVM can access it from "outside"
+    //     - static: so it can be called without creating an object (class scoped)
+    //     - void: it doesn't return a value (aka procedure)
+    //     - main: the required method name
+    //     - String[] args: so it can receive command-line arguments
+    public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in); // Listen to the standard input (console)
+		System.out.print("> "); // Prompt
+		while (true) { // Infinite loop
+			String command = scanner.nextLine(); // Get input from console as a string
+			String output = ""; // A variable named output of type String
+			if (command.equals("exit")) {
+				break; // Forces exit of the while loop
+			} else {
+				// String concatenation
+				output = "Command '" + command + "' not found.";
+			}
+			System.out.println(output); // Print with new line (ln)
+			System.out.print("> "); // Prompt
+		}
+		scanner.close(); // Best practice, always close a stream when no more needed
+		System.out.println("Bye!");
+    }
+
+}
+
